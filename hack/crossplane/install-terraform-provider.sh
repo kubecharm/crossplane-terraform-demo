516f4250dbd8a649a4babba0ffbac41c6b419ee8
mewa <marcin.k.chmiel@gmail.com>
2023-06-02 14:07:05 +0200

    Add crossplane init script

---
+++ hack/crossplane/install-terraform-provider.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eo pipefail
+
+BASE="$(readlink -f "$(dirname $0)")"
+while ! kubectl api-resources | grep pkg.crossplane.io; do sleep 1; done
+
+kubectl apply -f "$BASE/../../crossplane/terraform-provider.yaml"
+kubectl wait --for=condition=Installed providers/provider-terraform
+kubectl wait --for=condition=Healthy providers/provider-terraform
+
+kubectl apply -f "$BASE/../../crossplane/terraform-provider-config.yaml"
+
+TERRAFORM_SERVICE_ACCOUNT=$(kubectl -n crossplane-system get sa -ojson | jq -r '.items | map(.metadata.name | select(startswith("provider-terraform"))) | .[0]')
+
+echo "Applying Terraform provider service account RBAC"
+<"$BASE/terraform-provider-rbac.yaml" sed "s/{{ provider-terraform-service-account }}/$TERRAFORM_SERVICE_ACCOUNT/g" | kubectl apply -f -

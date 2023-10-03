7fadddf2060d419938c1fe7e34abc8deb1b582a7
mewa <marcin.k.chmiel@gmail.com>
2023-10-03 14:47:42 +0200

    Tidy up Terraform Provider setup

--- hack/crossplane/install-terraform-provider.sh
+++ hack/crossplane/install-terraform-provider.sh
@@ -2,13 +2,15 @@
 set -eo pipefail
 
 BASE="$(readlink -f "$(dirname $0)")"
+PROVIDER="$BASE/../../crossplane/provider"
+
 while ! kubectl api-resources | grep pkg.crossplane.io; do sleep 1; done
 
-kubectl apply -f "$BASE/../../crossplane/terraform-provider.yaml"
+kubectl apply -f "$PROVIDER/terraform-provider.yaml"
 kubectl wait --for=condition=Installed providers/provider-terraform
 kubectl wait --for=condition=Healthy providers/provider-terraform
 
-kubectl apply -f "$BASE/../../crossplane/terraform-provider-config.yaml"
+kubectl apply -f "$PROVIDER/terraform-provider-config.yaml"
 
 TERRAFORM_SERVICE_ACCOUNT=$(kubectl -n crossplane-system get sa -ojson | jq -r '.items | map(.metadata.name | select(startswith("provider-terraform"))) | .[0]')
 
